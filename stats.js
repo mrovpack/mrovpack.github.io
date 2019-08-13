102e446038b1caae08070f29bf4f00a86fa7a6da
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2019-08-14 01:15:47 +0200

    Update stats.js

--- stats.js
+++ stats.js
@@ -1,22 +1,30 @@
+var outputJSON = {};
+var scoreboardJSON;
+
 $.getJSON('https://andreymrovol.github.io/mrovpack-scoreboardData/scoreboard.json', function(status) {
 	console.log(status);
-
+	scoreboardJSON = status;
 
   var objectives = status.ObjectiveList;
 	var players = status.PlayerList;
 
-	var outputJSON = {};
-
 	for(var i=0; i<players.length; i++){
 		var a = players[i];
 
-		for(var n=0; n<objectives.length; n++){
-			var b = objectives[n];
-			var c = b[a];
-			console.log(a)
+		var d = outputJSON[a];
 
+		for(var n=0; n<objectives.length; n++){
+			var objectiveName = objectives[n];
+
+			if(status[objectiveName][a] !== 'undefined'){
+
+				var score = status[objectiveName][a];
+				d[objectiveName] = score;
+			}
 		}
 
 
 	}
 });
+
+console.warn(outputJSON)
