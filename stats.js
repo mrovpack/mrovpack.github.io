bdc877857b148558209acf862e2c3f20db73fec3
AndreyMrovol <AndreyMrovol@users.noreply.github.com>
2019-08-17 14:22:18 +0200

    release

--- stats.js
+++ stats.js
@@ -6,81 +6,92 @@ $.getJSON('https://andreymrovol.github.io/mrovpack-scoreboardData/scoreboard.jso
 	console.log(status);
 	scoreboardJSON = status;
 
-  var objectives = status.ObjectiveList;
-	var players = status.PlayerList;
+  // var objectives = status.ObjectiveList;
+	// var players = status.PlayerList;
+	//
+	// objectives.splice(objectives.indexOf('health'), 1);
+	// objectives.splice(objectives.indexOf('death'), 1);
+	//
+	// for(var i=0; i<players.length; i++){
+	// 	var a = players[i];
+	//
+	// 	outputJSON[a] = {};
+	// 	var d = outputJSON[a];
+	//
+	// 	for(var n=0; n<objectives.length; n++){
+	// 		var objectiveName = objectives[n];
+	//
+	// 		if(typeof status[objectiveName][a] != 'undefined'){
+	//
+	// 			d[objectiveName] = status[objectiveName][a];
+	// 		}
+	//
+	// 	}
+	//
+	// }
 
-	objectives.splice(objectives.indexOf('helath'), 1);
-	objectives.splice(objectives.indexOf('death'), 1);
-
-	for(var i=0; i<players.length; i++){
-		var a = players[i];
-
-		outputJSON[a] = {};
-		var d = outputJSON[a];
-
-		for(var n=0; n<objectives.length; n++){
-			var objectiveName = objectives[n];
-
-			if(typeof status[objectiveName][a] != 'undefined'){
-
-				d[objectiveName] = status[objectiveName][a];
-			}
-
-		}
-
-	}
-
-	outputJSON['objectiveList'] = objectives;
-//	document.getElementById('tabelka').appendChild(makeUL(outputJSON))
+	dataReady(status);
+	lookFor('Diamenty');
 });
 
-console.warn(outputJSON)
+function dataReady(json){
+	scoreboardJSON = json;
+}
 
+function lookFor(objective){
+	let arr = scoreboardJSON[objective];
+
+	document.getElementById('type').innerHTML = objective;
+
+	document.getElementById('tabelka').innerHTML = ''
+	document.getElementById('tabelka').appendChild(makeUL(arr));
+}
 
 function makeUL(array) {
 	console.log(array);
 		var table = document.createElement("table");
 		var tbody = document.createElement('tbody');
 
-		let trow = document.createElement('tr');
-		// trow.appendChild(document.createTextNode('Gracz'));
+		// let trow = document.createElement('tr');
+		//
+		// let thP = document.createElement('th');
+		// thP.innerHTML = 'Player'
+		// trow.appendChild(thP);
+		//
+		// let thS = document.createElement('th');
+		// thS.innerHTML = "Score"
+		// trow.appendChild(thS);
+		//
+		// tbody.appendChild(trow);
 
-		for(var i=0; i<array.objectiveList.length; i++){
-			let th = document.createElement('th');
-			th.innerHTML = array.objectiveList[i];
-			trow.appendChild(th);
+	//	table.style.width = '100%';
+		table.setAttribute('align', 'center')
+		// table.setAttribute('border', '1');
+
+		function getKeyByValue(object, value) {
+		return Object.keys(object).find(key => object[key] === value);
 		}
 
-		tbody.appendChild(trow);
-
-		table.style.width = '100%';
-		table.setAttribute('border', '1');
-
-		var players = Object.keys(array)
+		 var players = Object.values(array)
+		 players.sort(function(a, b){return b - a});
 
 		for (var i = 0; i < players.length; i++) {
 			var a = players[i]
 
-			var ks = array[a];
-			console.warn(ks)
+			let player = getKeyByValue(array, a);
+			let score = a;
 
 				var tr = document.createElement('tr');
+				var tdP = document.createElement('td');
+				var tdS = document.createElement('td');
 
-				var scores = Object.values(ks)
+				tdS.style.color = '#ff5555';
 
-				for(var n=0; n<scores.length; n++){
-					var td = document.createElement('td');
+				tdP.appendChild(document.createTextNode(player))
+				tdS.appendChild(document.createTextNode(score))
 
-					console.log(typeof scores[n])
-
-					if(typeof scores[n] == 'undefined'){
-						td.appendChild(document.createTextNode('0'))
-					} else{
-						td.appendChild(document.createTextNode(scores[n]))
-					}
-
-					tr.appendChild(td);
-				}
+				tr.appendChild(tdP)
+				tr.appendChild(tdS)
 
 				tbody.appendChild(tr);
 		}
