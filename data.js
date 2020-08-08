ceadce6354349958c2bcd8ed2efcd22b1cf294d6
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2020-08-08 20:45:58 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,3 +1,3 @@
 let IpServer = "mrovol.mine.game";
-let UrlScoreboard = "hhttps://mrov.dev/mrovpack-scoreboardData/scoreboard.json";
+let UrlScoreboard = "https://mrov.dev/mrovpack-scoreboardData/scoreboard.json";
 let UrlImages = "https://mrovtest.github.io/iso/";
