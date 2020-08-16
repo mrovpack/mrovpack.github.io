88885f7fbb97d51326ea31d85dcfe9e6812432a3
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2020-08-16 23:08:25 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,19 +3,7 @@ self.addEventListener('install', function(e) {
    caches.open('site').then(function(cache) {
      return cache.addAll([
        './',
-       './index.html',
-       './index.js',
-       './stats/index.html',
-       './stats/index.js',
-       './stats/player/index.html',
-       './stats/player/index.js',
-       './wiki/index.html',
-       './wiki/index.js',
-       './wiki/index.json',
-       'https://mrov.dev/mrovpack-scoreboardData/scoreboard.json',
-       'https://mrovtest.github.io/sd/statbuttons.json',
-       'https://andreymrovol.github.io/resources/style.css',
-       'https://mrovpack.github.io/assets/icon.png'
+       './index.html'
      ]);
    })
  );
