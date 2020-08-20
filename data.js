839ba2dfbb141b90894493ca50f4a413369ba7d8
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2020-08-20 21:36:22 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,3 +1,5 @@
 let IpServer = "server.mrov.dev";
 let UrlScoreboard = "https://mrov.dev/mrovpack-scoreboardData/scoreboard.json";
 let UrlImages = "https://pack.mrov.dev/assets/renders/";
+let UrlHeads = "https://mrov.dev/mrovpack-players/"
+let HeadDefault = "https://mrov.dev/mrovpack-players/default.png"
